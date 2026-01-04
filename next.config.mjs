f6f1abc78dbe639e049f9280b87a11f13e116833
Gaurav Koli <101950880+Gk091202@users.noreply.github.com>
2026-01-04 22:37:23 +0530

    error removed

--- next.config.mjs
+++ next.config.mjs
@@ -18,24 +18,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cloud.appwrite.io",
-        pathname: "/**",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "2mb",
-    },
-  },
-};
-
-export default nextConfig;
